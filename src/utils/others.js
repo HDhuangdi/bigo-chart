249ff2dc7fe039789f0fa4af5d3fcfaa67495f1b
htwoO <1549914423@qq.com>
2021-06-03 15:06:11 +0800

    fix:鼠标定位蜡烛图位置偏移微差问题

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,31 +1,31 @@
-export function dateFormat(fmt, date) {
+export function dateFormat (fmt, date) {
   let ret
   const opt = {
-    "Y+": date.getFullYear().toString(), // 年
-    "m+": (date.getMonth() + 1).toString(), // 月
-    "d+": date.getDate().toString(), // 日
-    "H+": date.getHours().toString(), // 时
-    "M+": date.getMinutes().toString(), // 分
-    "S+": date.getSeconds().toString(), // 秒
+    'Y+': date.getFullYear().toString(), // 年
+    'm+': (date.getMonth() + 1).toString(), // 月
+    'd+': date.getDate().toString(), // 日
+    'H+': date.getHours().toString(), // 时
+    'M+': date.getMinutes().toString(), // 分
+    'S+': date.getSeconds().toString() // 秒
     // 有其他格式化字符需求可以继续添加，必须转化成字符串
   }
   for (const k in opt) {
-    ret = new RegExp("(" + k + ")").exec(fmt)
+    ret = new RegExp('(' + k + ')').exec(fmt)
     if (ret) {
       fmt = fmt.replace(
         ret[1],
-        ret[1].length === 1 ? opt[k] : opt[k].padStart(ret[1].length, "0")
+        ret[1].length === 1 ? opt[k] : opt[k].padStart(ret[1].length, '0')
       )
     }
   }
   return fmt
 }
 
-export function warn(msg) {
-  throw new Error("[bigo-chart warn]:" + msg)
+export function warn (msg) {
+  throw new Error('[bigo-chart warn]:' + msg)
 }
 
-export function mergeObject(oldObj, newObj) {
+export function mergeObject (oldObj, newObj) {
   for (const key in newObj) {
     oldObj[key] = newObj[key]
   }
